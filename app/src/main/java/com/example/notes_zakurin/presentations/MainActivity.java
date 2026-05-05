7158daf093c00cceef8af9ab3efec038dad38a09
danil <danil@DESKTOP-T4KQBOA>
2026-05-05 16:59:11 +0500

    программирование окна NoteActivity закурин

--- app/src/main/java/com/example/notes_zakurin/presentations/MainActivity.java
+++ app/src/main/java/com/example/notes_zakurin/presentations/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.notes_zakurin.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/notes_zakurin/presentations/NoteActivity.java
+++ app/src/main/java/com/example/notes_zakurin/presentations/NoteActivity.java
@@ -1,6 +1,11 @@
 package com.example.notes_zakurin.presentations;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +14,79 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.notes_zakurin.R;
+import com.example.notes_zakurin.datas.RepoNotes;
+import com.example.notes_zakurin.domains.models.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NoteActivity extends AppCompatActivity {
+
+    Note note;
+
+    EditText etTitle, etText;
+    TextView tvDate;
+    View btnSelectColor, btnBack, btnTrash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_note);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Date DateNow = new Date();
+        SimpleDateFormat FormatForDateNow = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+
+        btnSelectColor = findViewById(R.id.btn_select_color);
+        btnBack = findViewById(R.id.btn_back);
+        btnTrash = findViewById(R.id.btn_trash);
+        etTitle = findViewById(R.id.et_title);
+        etText = findViewById(R.id.et_text);
+        tvDate = findViewById(R.id.tv_date);
+
+        Bundle arguments = getIntent().getExtras();
+        if (arguments != null) {
+            int Position = arguments.getInt("position");
+
+            note = RepoNotes.Notes.get(Position);
+
+            etTitle.setText(note.title);
+            etText.setText(note.text);
+        } else {
+            btnTrash.setVisibility(View.GONE);
+        }
+        tvDate.setText("Отредактировано: " + FormatForDateNow.format(DateNow));
+
+        btnSelectColor.setOnClickListener(v -> {
+            Toast.makeText(this, "Выбор цвета недоступен", Toast.LENGTH_SHORT).show();
+        });
+
+        btnBack.setOnClickListener(v -> {
+            String Title = etTitle.getText().toString();
+            String Text = etText.getText().toString();
+
+            if(Text
+                    .replace(" ", "")
+                    .replace("\r", "")
+                    .replace("\n", "")
+                    .isEmpty()){
+                Toast.makeText(this, "Нечего сохранять", Toast.LENGTH_SHORT).show();
+            } else {
+                if(note == null) {
+                    note = new Note();
+                    RepoNotes.Notes.add(note);
+                }
+
+                note.title = Title;
+                note.text = Text;
+                note.date = FormatForDateNow.format(DateNow);
+            }
+            finish();
+        });
+
+        btnTrash.setOnClickListener(v -> {
+            RepoNotes.Notes.remove(note);
+            finish();
+            Toast.makeText(this, "Заметка удалена", Toast.LENGTH_SHORT).show();
         });
     }
 }
